67ab20adc80a09e6aa435fb18797638c6a1dc661
Michael Wilson <mikallwilsonn@gmail.com>
2020-06-16 09:22:51 -0400

    Completed 'Interval/Dynamic System Stats'

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -7,6 +7,8 @@ const mem = osu.mem;
 const os = osu.os;
 
 
+// ----
+// Static Stats
 
 // Set Model
 document.querySelector( '#cpu-model' ).innerText = cpu.model();
@@ -21,3 +23,35 @@ document.querySelector( '#os' ).innerText = `${ os.type() } (${ os.arch() })`;
 mem.info().then(( info ) => {
     document.querySelector( '#mem-total' ).innerText = info.totalMemMb;
 });
+
+
+// ----
+// Dynamic Stats / Interval
+setInterval(() => {
+    // CPU Usage
+    cpu.usage().then(( info ) => {
+        document.querySelector( '#cpu-usage' ).innerText = `${ info }%`;
+    });
+
+    // CPU Free
+    cpu.free().then(( info ) => {
+        document.querySelector( '#cpu-free' ).innerText = `${ info }%`;
+    });
+
+    // Uptime
+    document.querySelector( '#sys-uptime' ).innerText = SecondsToDHMS( os.uptime() );
+
+}, 2000 );
+
+
+// Show, days, hours, mins, and sec for uptime
+function SecondsToDHMS( value ) {
+    value = +value;
+
+    const day = Math.floor( value / ( 3600 * 24 ));
+    const hour = Math.floor(( value % ( 3600 * 24 )) / 3600 );
+    const minutes = Math.floor(( value % 3600 ) / 60 );
+    const seconds = Math.floor( value % 60 );
+
+    return `${ day }d, ${ hour }h, ${ minutes }m, ${ seconds }s`;
+}
